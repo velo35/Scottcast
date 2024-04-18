7198de6ddcf8f5f9cf99e85103d846da4d042d90
Scott Daniel <sadaniel35@gmail.com>
2024-04-18 16:36:30 -0500

    Working MovingTextView

--- Scottcast/Views/Components/MovingTextView.swift
+++ Scottcast/Views/Components/MovingTextView.swift
@@ -7,111 +7,82 @@
 
 import SwiftUI
 
-class MovingText: UIView
+private struct MovingTextPreference: PreferenceKey
 {
-    let first = UILabel()
-    let second = UILabel()
-    
-    var startTimer: Timer?
-    var animationTimer: Timer?
-    
-    let gap = 20.0
-    
-    convenience init(text: String)
-    {
-        self.init(frame: .zero)
-        
-        for label in [first, second] {
-            label.text = text
-            label.translatesAutoresizingMaskIntoConstraints = false
-            label.sizeToFit()
-        }
-        self.addSubview(self.first)
-        self.clipsToBounds = true
-    }
-    
-    var text: String
-    {
-        get {
-            self.first.text ?? ""
-        }
-        set {
-            for label in [first, second] {
-                label.text = newValue
-                label.sizeToFit()
-            }
-            self.update()
-        }
-    }
-    
-    func update()
-    {
-        self.startTimer?.invalidate()
-        self.animationTimer?.invalidate()
-        
-        if self.frame.width < first.frame.width {
-            self.startTimer = Timer.scheduledTimer(withTimeInterval: 2, repeats: false) { [unowned self] _ in
-                self.animationTimer = Timer.scheduledTimer(withTimeInterval: 1.0 / 30.0, repeats: true) { [unowned self] _ in
-                    self.bounds.origin.x = (self.bounds.origin.x + 1).truncatingRemainder(dividingBy: self.first.frame.width + self.gap)
-                }
-            }
-            
-            self.addSubview(self.second)
-            self.second.frame.origin.x = self.first.frame.size.width + self.gap
-        }
-        else {
-            self.mask = nil
-            self.second.removeFromSuperview()
-        }
-    }
+    static let defaultValue = false
+    static func reduce(value: inout Bool, nextValue: () -> Bool) {}
 }
 
-struct _MovingTextView: UIViewRepresentable
+private struct MovingTextWidthPreference: PreferenceKey
 {
-    let text: String
-    
-    func makeUIView(context: Context) -> MovingText
-    {
-        MovingText(text: text)
-    }
-    
-    func updateUIView(_ uiView: MovingText, context: Context)
-    {
-        uiView.text = text
-    }
-    
-    func sizeThatFits(_ proposal: ProposedViewSize, uiView: MovingText, context: Context) -> CGSize?
-    {
-        guard let width = proposal.width, !width.isInfinite && !width.isZero else { return nil }
-        let height = uiView.first.frame.height
-        uiView.frame = CGRect(origin: .zero, size: CGSize(width: width, height: height))
-        uiView.update()
-        return CGSize(width: width, height: height)
-    }
-    
-    static func dismantleUIView(_ uiView: MovingText, coordinator: ())
-    {
-        uiView.startTimer?.invalidate()
-        uiView.animationTimer?.invalidate()
-    }
+    static let defaultValue = CGFloat.zero
+    static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {}
 }
 
 struct MovingTextView: View
 {
     let text: String
     
+    @State private var shouldMove = false
+    @State private var textWidth = CGFloat.zero
+    @State private var offset = CGFloat.zero
+    
+    let gap = CGFloat(20)
+    
     var body: some View
     {
-        _MovingTextView(text: text)
+        Text("A")
+            .frame(maxWidth: .infinity)
+            .hidden()
+            .background(alignment: .leading) {
+                GeometryReader { proxy in
+                    Text(text)
+                        .fixedSize()
+                        .background {
+                            GeometryReader { textProxy in
+                                Color.clear
+                                    .preference(key: MovingTextWidthPreference.self, value: textProxy.size.width)
+                                    .onPreferenceChange(MovingTextWidthPreference.self) { value in
+                                        textWidth = textProxy.size.width
+                                    }
+                                    .preference(key: MovingTextPreference.self, value: proxy.size.width < textProxy.size.width)
+                                    .onPreferenceChange(MovingTextPreference.self) { value in
+                                        shouldMove = value
+                                        offset = CGFloat.zero
+                                    }
+                                    .onChange(of: shouldMove) {
+                                        if shouldMove {
+                                            Task {
+                                                try? await Task.sleep(nanoseconds: 3_000_000_000)
+                                                offset = -(textWidth + gap)
+                                            }
+                                        }
+                                    }
+                            }
+                        }
+                        .background {
+                            Text(text)
+                                .offset(x: textWidth + gap)
+                        }
+                        .offset(x: offset)
+                        .animation(.linear(duration: 8).repeatCount(3, autoreverses: false), value: offset)
+                }
+            }
+            .clipped()
             .mask {
-                LinearGradient(
-                    stops: [
-                        .init(color: .white, location: 0.75),
-                        .init(color: .clear, location: 1)
-                    ],
-                    startPoint: .leading,
-                    endPoint: .trailing
-                )
+                if shouldMove {
+                    LinearGradient(
+                        stops: [
+                            .init(color: .white, location: 0.75),
+                            .init(color: .clear, location: 1)
+                        ],
+                        startPoint: .leading,
+                        endPoint: .trailing
+                    )
+                }
+                else {
+                    Color.black
+                }
             }
     }
 }
