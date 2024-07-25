22d264b2bff4f622cba2627019bbf47d9406f86a
Scott Daniel <sadaniel35@gmail.com>
2024-07-25 16:05:43 -0700

    Update pocast on appear of PodcastView

--- Scottcast/Views/Podcast/PodcastView.swift
+++ Scottcast/Views/Podcast/PodcastView.swift
@@ -8,11 +8,27 @@
 import SwiftUI
 import SwiftData
 
-struct PodcastView: View 
+@MainActor
+struct PodcastView: View
 {
     @Environment(\.modelContext) private var modelContext
     
     let podcast: Podcast
+    
+    private func updatePodcast() async
+    {
+        do {
+            let lookup = try await NetworkService.lookup(podcastId: podcast.id)
+            let newEpisodeInfos = lookup.episodes.filter{ info in !self.podcast.episodes.contains(where: { $0.id == info.id }) }
+            let newEpisodes = newEpisodeInfos.map{ Episode(from: $0) }
+            for episode in newEpisodes {
+                self.modelContext.insert(episode)
+                episode.podcast = podcast
+            }
+        } catch {
+            debugPrint(error.localizedDescription)
+        }
+    }
     
     var body: some View
     {
@@ -40,6 +56,11 @@ struct PodcastView: View
             
             ForEach(podcast.sortedEpisodes) { episode in
                 EpisodeCellView(episode: episode)
+            }
+        }
+        .onAppear {
+            Task {
+                await self.updatePodcast()
             }
         }
         .refreshable {
